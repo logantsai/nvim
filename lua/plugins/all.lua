4c07c0dae89088577e3d5195f72ff5d7d0c647d0
MarkTsai <marktsai0213@gmail.com>
2024-06-16 21:06:35 +0800

    feat: add nvim-surround and mini.tabline

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -2,27 +2,24 @@ return {
   {
     "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {},
     init = function()
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme "tokyonight-night"
     end,
   },
 
   { "catppuccin/nvim", name = "catppuccin", priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme "catppuccin"
     end,
   },
 
-  { 'folke/todo-comments.nvim',
+  { "folke/todo-comments.nvim",
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false }
   },
 
   {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {},
-    config = true
+    "windwp/nvim-autopairs", event = "InsertEnter", opts = {}, config = true
   },
 
   {
@@ -30,6 +27,13 @@ return {
     config = function()
       require("mini.trailspace").setup({}) -- highlight and remove trailing spaces
       require("mini.indentscope").setup({})
+      require("mini.tabline").setup({})
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+      map('n', ']b', '<Cmd>tabNext<CR>', "Next Tab")
+      map('n', '[b', '<Cmd>tabprevious<CR>', "Prev Tab")
     end,
   },
 
@@ -62,6 +66,13 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
     opts = {},
   },
 
